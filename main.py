f65fa2310b83994bd45a4887a7b4d07fb1adc9e9
Jorge Nachtigall <geevazz@hotmail.com>
2019-03-14 14:49:22 -0300

    optional params to listBlocks, added listActors and listCostumes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 
-
 class Analyscratch:
     def __init__(self, filename):
         self.filename = filename
@@ -8,17 +7,37 @@ class Analyscratch:
             json_data = json.load(json_file)
         self.data_targets = json_data['targets']
 
-    def listBlocks(self):
+    def listBlocks(self, actorName = None):
         for actor in self.data_targets:
-            if(actor['blocks']):
-                for blocks in actor['blocks']:
-                    if type(actor['blocks'][blocks]) is dict:
-                        block = actor['blocks'][blocks]
-                        print(block['opcode'])
-                    else:
-                        block = actor['blocks'][blocks]
-                        print('variable_' + block[1])
+            if(actorName):
+                if(actor['name'] == actorName and actor['blocks']):
+                    for blocks in actor['blocks']:
+                        if type(actor['blocks'][blocks]) is dict:
+                            block = actor['blocks'][blocks]
+                            print(block['opcode'])
+                        else:
+                            block = actor['blocks'][blocks]
+                            print('variable_' + block[1])
+            else:
+                if(actor['blocks']):
+                    for blocks in actor['blocks']:
+                        if type(actor['blocks'][blocks]) is dict:
+                            block = actor['blocks'][blocks]
+                            print(block['opcode'])
+                        else:
+                            block = actor['blocks'][blocks]
+                            print('variable_' + block[1])
 
-
-block_list = Analyscratch('project.json')
-block_list.listBlocks()
+    def listActors(self):
+        for actor in self.data_targets:
+            print(actor['name'])
+    
+    def listCostumes(self, actorName = None):
+        for actor in self.data_targets:
+            if(actorName):
+                if(actor['name'] == actorName):
+                    for costumes in actor['costumes']:
+                        print(costumes['name'])
+            else:
+                for costumes in actor['costumes']:
+                    print(costumes['name'])
